8f3f4994ebaca82e5f21c8ac3beba6504337f6e7
Jiheui <developer.jiheuilee@gmail.com>
2024-03-15 16:15:52 +0900

    added controllers and views

--- src/main/java/com/gdu/pro2/controller/MyController2.java
+++ src/main/java/com/gdu/pro2/controller/MyController2.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MyController2 {
 
   @RequestMapping(value="/board/list.do", method= RequestMethod.GET)
-    public String method(){
-      return "index2";
+    public void method(){
+
   }
 
 }
